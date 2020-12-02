bd59ad6ee4e191009ac18e72e44cb8ab79d4f882
Kaushik <kaushik.subramanian@ul.com>
2020-12-02 17:30:17 -0500

    First commit

---
+++ src/test/java/com/springboot/testproject/TestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
